255e59ff0b48c4d1a5241b81a52067fcbe7fd5a9
ElTinmar <martin.privat@hotmail.fr>
2023-11-02 10:58:51 +0100

    add case

--- test.py
+++ test.py
@@ -2,18 +2,40 @@ from logger import Logger
 import logging
 from multiprocessing import Process
 
-def other_process(logger: Logger):
+def add_info_entry(logger: Logger):
     logger.configure_emitter()
     local_logger = logger.get_logger('here')
     local_logger.log(logger.INFO, 'log entry')
 
+def add_debug_entry(logger: Logger):
+    logger.configure_emitter()
+    local_logger = logger.get_logger('there')
+    local_logger.log(logger.DEBUG, 'other log entry')
+
 if __name__ == '__main__':
-    # create the object
+
     logger = Logger()
     logger.start()
     
-    p = Process(target=other_process, args=(logger,))
+    p = Process(target=add_info_entry, args=(logger,))
+    p0 = Process(target=add_debug_entry, args=(logger,))
+    p0.start()
     p.start()
     p.join()
+    p0.join()
 
     logger.stop()
+
+    # -------------------------------------------------------
+
+    logger2 = Logger('log2.txt', listener_level=logging.WARNING)
+    logger2.start()
+    
+    p = Process(target=add_info_entry, args=(logger2,))
+    p0 = Process(target=add_debug_entry, args=(logger2,))
+    p0.start()
+    p.start()
+    p.join()
+    p0.join()
+
+    logger2.stop()
