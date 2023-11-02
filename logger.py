9efafe9d20deaa0e28379bda860ccb97a6cf896a
ElTinmar <martin.privat@hotmail.fr>
2023-11-02 10:58:39 +0100

    no wait

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import logging
 import logging.handlers
 from multiprocessing import Process, Queue, Event
-
+from queue import Empty
 class Logger(Process):
     '''
     Simple multiprocessing logger class to handle logging from multiple processes to the same 
@@ -15,21 +15,23 @@ class Logger(Process):
     ERROR = logging.ERROR
     CRITICAL = logging.CRITICAL
 
-    def __init__(self, filename: str = 'log.txt', *args, **kwargs):
+    def __init__(self, filename: str = 'log.txt', listener_level = logging.DEBUG, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.filename = filename
         self.queue = Queue()
         self.stop_evt = Event()
+        self.listener_level = listener_level
 
     def configure_listener(self) -> None:
         '''
         Configure root logger for the listener process
         '''
         root = logging.getLogger()
-        handler = logging.FileHandler(self.filename, 'a')
+        handler = logging.FileHandler(self.filename, 'w')
         formatter = logging.Formatter('%(asctime)s %(processName)-10s %(name)s %(levelname)-8s %(message)s')
         handler.setFormatter(formatter)
         root.addHandler(handler)
+        root.setLevel(self.listener_level)
 
     def configure_emitter(self, level = logging.DEBUG) -> None:
         '''
@@ -54,11 +56,14 @@ class Logger(Process):
 
         self.configure_listener()
         while not self.stop_evt.is_set():
-            record = self.queue.get()
-            if record is None:
-                break
-            logger = logging.getLogger(record.name)
-            logger.handle(record)
+            try:
+                record = self.queue.get_nowait()
+                if record is None:
+                    break
+                logger = logging.getLogger(record.name)
+                logger.handle(record)
+            except Empty: # should I sleep to avoid CPU usage ?
+                pass
 
     def stop(self):
         '''
