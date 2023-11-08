a6cf514e636f992b2245807c28c39381bb8d33b8
ElTinmar <martin.privat@hotmail.fr>
2023-11-08 14:56:02 +0100

    adding file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='multiprocessing_logger',
+    author='Martin Privat',
+    version='0.1',
+    packages=['multiprocessing_logger','multiprocessing_logger.tests'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='log from separate processes into a single file',
+    long_description=open('README.md').read(),
+    install_requires=[]
+)
