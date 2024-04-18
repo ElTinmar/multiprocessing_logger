7d91fcb143b77aa10f08d636800a85de82997c62
ElTinmar <martin.privat@hotmail.fr>
2024-04-18 21:01:09 +0200

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='multiprocessing_logger',
     author='Martin Privat',
-    version='0.2.6',
+    version='0.2.7',
     packages=['multiprocessing_logger','multiprocessing_logger.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='log from separate processes into a single file',
