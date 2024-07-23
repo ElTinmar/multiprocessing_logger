a1280daa94d2473d5687c432b63bc8c745bb8255
ElTinmar <martin.privat@hotmail.fr>
2024-07-24 00:01:24 +0200

    clearing handler

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='multiprocessing_logger',
     author='Martin Privat',
-    version='0.3.2',
+    version='0.3.3',
     packages=['multiprocessing_logger','multiprocessing_logger.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='log from separate processes into a single file',
